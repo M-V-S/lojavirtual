f187ac82ac0eceedf53508b9871ddca551a02219
M-V-S <maicon-santosnex@hotmail.com>
2018-10-11 13:05:19 -0300

    estruturar pagina do admin

--- index.php
+++ index.php
@@ -5,19 +5,29 @@ require_once("vendor/autoload.php");
 $app = new \Slim\Slim();
 
 use \Hcode\Page;
+use \Hcode\PageAdmin;
+
+
 
 $app->config('debug', true);
 
+//solicitar pagina principal do site
 $app->get('/', function() {
-
-	
 
     $page = new Page();
 
     $page->setTpl("index");
+});
 
+//solicitar pagina  inicial do administrador
+$app->get('/admin', function(){
+	$pageAdmin = new PageAdmin();
+	$pageAdmin->setTpl("index");
 
 });
+
+
+
 
 $app->run();
 
