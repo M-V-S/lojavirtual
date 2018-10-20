a1ab7a537be512896f84b9e80fd4ab36b66c183b
M-V-S <maicon-santosnex@hotmail.com>
2018-10-19 22:57:05 -0300

    categorias user

--- index.php
+++ index.php
@@ -231,96 +231,117 @@ $app->post('/admin/forgot/reset', function(){
 	/*------------------END CRUD USER-----------------------*/
 
 	/*------------------CRUD CATEGORIAS-----------------------*/
-	$app->get('/admin/categories', function(){
-		User::verifyLogin();
-		
-		
-		$categories = Category::listAll();
-
-		$pageAdmin = new PageAdmin();
-
-		$pageAdmin->setTpl("categories", array(
-			"categories"=>$categories
-		));
-
-		exit();	
-	});
-
-	$app->get('/admin/categories/create', function(){
-		User::verifyLogin();
-		$pageAdmin = new PageAdmin();
-
-		$pageAdmin->setTpl("categories-create");
-
-		exit();		
-	});
-
-
-
-	//receber post 
-	$app->post('/admin/categories/create', function(){
-		User::verifyLogin();
-
-		$category = new Category();
-
-		$category->setData($_POST);
-
-		$category->save();
-
-		header("Location: /admin/categories");
-		exit();
-
-	});
-
-	$app->get('/admin/categories/:idcategory/delete', function($idcategory){
-		User::verifyLogin();
-
-		$category = new Category();
-
-		$category->get((int)$idcategory);
-
-		$category->delete();
-
-		header("Location: /admin/categories");
-		exit();
-	});
-
-	$app->get('/admin/categories/:idcategory', function($idcategory){
-		User::verifyLogin();
-
-		$category = new Category();
-
-		$category->get((int)$idcategory);
-
-		$pageAdmin = new PageAdmin();
-
-		$pageAdmin->setTpl("categories-update", array(
-			"category"=>$category->getValues()
-		));
-
-		exit();	
-	});
-
-	$app->post('/admin/categories/:idcategory', function($idcategory){
-		User::verifyLogin();
-
-		$category = new Category();
-
-		$category->get((int)$idcategory);
-
-		$category->setData($_POST);
-
-		$category->save();
-
-		header("Location: /admin/categories");
-
-		exit();	
-	});
-
-
-
-
+$app->get('/admin/categories', function(){
+	User::verifyLogin();
 	
+	
+	$categories = Category::listAll();
+
+	$pageAdmin = new PageAdmin();
+
+	$pageAdmin->setTpl("categories", array(
+		"categories"=>$categories
+	));
+
+	exit();	
+});
+
+$app->get('/admin/categories/create', function(){
+	User::verifyLogin();
+	$pageAdmin = new PageAdmin();
+
+	$pageAdmin->setTpl("categories-create");
+
+	exit();		
+});
+
+
+
+//receber post 
+$app->post('/admin/categories/create', function(){
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit();
+
+});
+
+$app->get('/admin/categories/:idcategory/delete', function($idcategory){
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit();
+});
+
+$app->get('/admin/categories/:idcategory', function($idcategory){
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$pageAdmin = new PageAdmin();
+
+	$pageAdmin->setTpl("categories-update", array(
+		"category"=>$category->getValues()
+	));
+
+	exit();	
+});
+
+$app->post('/admin/categories/:idcategory', function($idcategory){
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+
+	exit();	
+});
+
+/*------------------CRUD END CATEGORIAS user-----------------------*/
+
+
+$app->get('/categories/:idcategory', function($idcategory){
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	 $page->setTpl("category", [
+	 	"category"=>$category->getValues()
+	 ]);
+	
+	 $category = new Category();
+
+	 $category->get((int)$idcategory);
+
+	$page = new Page();
+    $page->setTpl("category", [
+    	"category"=> $category->getValues(),
+    	"products"=>$products[]
+    ]);
+});
+
 
 
 $app->run();
