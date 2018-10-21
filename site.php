fab9505f7a5c0314db0f1289928ee24b802b0322
M-V-S <maicon-santosnex@hotmail.com>
2018-10-21 06:56:53 -0300

    mostrar produtos front end

--- site.php
+++ site.php
@@ -1,29 +1,24 @@
 <?php
 use \Hcode\Page;
 use \Hcode\Model\Category;
+use \Hcode\Model\Product;
 
 
 //solicitar pagina principal do site
 $app->get('/', function() {
+	$Product = Product::listAll();
 
     $page = new Page();
 
-    $page->setTpl("index");
+    $page->setTpl("index", [
+    	"products"=>Product::checkList($Product)
+    ]);
 });
 
 
 //CATEGORIA STORE
 $app->get('/categories/:idcategory', function($idcategory){
-	$category = new Category();
 
-	$category->get((int)$idcategory);
-
-	$page = new Page();
-
-	 $page->setTpl("category", [
-	 	"category"=>$category->getValues()
-	 ]);
-	
 	 $category = new Category();
 
 	 $category->get((int)$idcategory);
@@ -34,6 +29,7 @@ $app->get('/categories/:idcategory', function($idcategory){
     	//"products"=>$products[]
     ]);
 });
+
 
 
 
